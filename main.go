fc537e770dba6865e99884aa63dc2040653bdcc3
junhyeok <toughhyeok96@gmail.com>
2023-04-07 00:22:16 +0900

    ./main run ls -l

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 )
 
 func main() {
@@ -16,4 +17,10 @@ func main() {
 
 func run() {
 	fmt.Printf("Running: %v\n", os.Args[2:])
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+
+	cmd.Run()
 }
