d32907bf9643de815b941e6f722f9a04bda389a7
Cloud User <ec2-user@ip-172-31-38-127.ap-northeast-2.compute.internal>
2023-04-07 12:42:54 +0000

    hostname := fake-container

--- main.go
+++ main.go
@@ -7,10 +7,19 @@ import (
 	"syscall"
 )
 
+// docker           run image <CMD> <ARG>
+// go run main.go   run       <CMD> <ARG>
+
+// Step4: 컨테이너 환경 시작시 호스트명을 container로 변경.
+// $ go run . run /bin/sh
+// $ hostname
+
 func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		os.Exit(1)
 	}
@@ -18,7 +27,8 @@ func main() {
 
 func run() {
 	fmt.Printf("Running: %v\n", os.Args[2:])
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	// cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -30,8 +40,22 @@ func run() {
 	must(cmd.Run())
 }
 
+func child() {
+	fmt.Printf("Running child: %v\n", os.Args[2:])
+
+	// hostname 설정
+	syscall.Sethostname([]byte("fake-container"))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+
+	must(cmd.Run())
+}
+
 func must(err error) {
 	if err != nil {
-		panic(err)d
+		panic(err)
 	}
 }
